2b369001d6b9441d1a5e124dbb1e1f8582975e8e
Aneta Kilar <235892@edu.p.lodz.pl>
2022-05-29 13:10:09 +0200

    Zadokowanie elementów okna

--- src/main/java/app/tapatan/AppController.java
+++ src/main/java/app/tapatan/AppController.java
@@ -8,13 +8,17 @@ import javafx.scene.layout.AnchorPane;
 
 import java.io.File;
 
+//TODO: skalowanie okna
+
 public class AppController {
     @FXML private AnchorPane root;
     @FXML private ImageView map;
+    @FXML private ImageView pane;
 
     @FXML
     public void initialize(){
         map.setImage(new Image(new File("src/main/resources/app/tapatan/arts/map_600.png").toURI().toString()));
+        pane.setImage(new Image(new File("src/main/resources/app/tapatan/arts/pane.png").toURI().toString()));
     }
 
     @FXML private Label playerNr;
