01322e8a1e83f51058c23c7adcd8b0a79e56b135
mateu <235953@edu.p.lodz.pl>
2022-05-31 22:59:31 +0200

    Dodanie metody wyświetlającej punkt

--- src/main/java/app/tapatan/classes/Point.java
+++ src/main/java/app/tapatan/classes/Point.java
@@ -3,4 +3,5 @@ package app.tapatan.classes;
 public class Point {
     public int x = 0;
     public int y = 0;
+    public void showPoint(){ System.out.println("(" + this.x + "," + this.y + ")"); }
 }
