aafc5cabffbd7191afa5e743872712f1e1b787a3
Krzych <kskrzysiek788@gmail.com>
2022-06-05 20:32:46 +0200

    3 testy + poprawka w GameLoop + dodanie JUnit w pom.xml (automatycznie)

---
+++ src/test/java/TapatanTests.java
@@ -0,0 +1,52 @@
+import app.tapatan.GameController;
+import app.tapatan.classes.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TapatanTests {
+    @Test
+    public void testBoardBoundary() {
+        Board board = new Board();
+        Point p = new Point();
+        boolean check;
+        for(int y = 0; y < 3; y++) {
+            p.y = y;
+            for(int x = 0; x < 3; x++) {
+                p.x = x;
+                check = Board.isOutOfBound(p);
+                Assert.assertFalse(check);
+            }
+        }
+        for(int x = 0; x < 3; x++) {
+            p.x = x;
+            p.y = x % 2 == 0 ? 4 : -1;
+            check = Board.isOutOfBound(p);
+            Assert.assertTrue(check);
+        }
+        for(int y = 0; y < 3; y++) {
+            p.y = y;
+            p.x = y % 2 == 0 ? 4 : -1;
+            check = Board.isOutOfBound(p);
+            Assert.assertTrue(check);
+        }
+    }
+
+    @Test
+    public void testIfEmptyType() {
+        Board board = new Board();
+        Tile tile;
+        for(int y = 0; y < 3; y++) {
+            for(int x = 0; x < 3; x++) {
+                tile = Board.tileTable[y][x];
+                Assert.assertSame(tile.tileType, TileType.TILE_EMPTY);
+            }
+        }
+    }
+
+    @Test
+    public void testNotWin() {
+        GameLoop.winCheck();
+        Assert.assertFalse(GameLoop.winConditionsFullfill);
+
+    }
+}
